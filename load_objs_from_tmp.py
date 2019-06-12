1f9472a479444a23b7495164e73d85dd29799b8a
ishbosamiya <ishbosamiya@gmail.com>
2019-06-13 01:23:44 +0530

    Load obj sequence from temp
    Loads obj sequence from /tmp/objs/ and sets keyframes to both viewport and render visibility so that it can be rendered or viewed.
    TODO:
    Add functionality to render the animation through script itself
    If possible find a way to run only python script to do everything, run Blender in the background

---
+++ load_objs_from_tmp.py
@@ -0,0 +1,38 @@
+import bpy
+import os
+
+paths = os.listdir("/tmp/objs")
+paths.sort()
+
+print(paths)
+
+for path in paths:
+    temp_path = "/tmp/objs/" + path
+    bpy.ops.import_scene.obj(filepath=temp_path)
+
+objects = list(bpy.data.objects)
+for object in objects:
+    if not bpy.data.meshes.get(object.name):
+        continue
+    object.hide_viewport = True
+    object.hide_render = True
+    
+frame = 1.0
+for object in objects:
+    if not bpy.data.meshes.get(object.name):
+        continue
+    bpy.context.view_layer.objects.active = object
+    bpy.context.active_object.keyframe_insert(data_path = "hide_render", frame = frame - 1.0)
+    bpy.context.active_object.keyframe_insert(data_path = "hide_viewport", frame = frame - 1.0)
+    bpy.context.view_layer.objects.active.hide_render = False
+    bpy.context.view_layer.objects.active.hide_viewport = False
+    bpy.context.active_object.keyframe_insert(data_path = "hide_render", frame = frame)
+    bpy.context.active_object.keyframe_insert(data_path = "hide_viewport", frame = frame)
+    bpy.context.view_layer.objects.active.hide_render = True
+    bpy.context.view_layer.objects.active.hide_viewport = True
+    bpy.context.active_object.keyframe_insert(data_path = "hide_render", frame = frame + 1.0)
+    bpy.context.active_object.keyframe_insert(data_path = "hide_viewport", frame = frame + 1.0)
+    frame += 1.0
+
+bpy.data.scenes['Scene'].frame_end = frame
+bpy.data.scenes['Scene'].frame_current = 1.0
