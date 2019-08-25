58742e81efe3bb8889cce4308b3812a5209ae8db
ishbosamiya <ishbosamiya@gmail.com>
2019-08-25 15:41:30 +0530

    Orientation and show wire

--- load_objs_from_tmp.py
+++ load_objs_from_tmp.py
@@ -8,7 +8,7 @@ print(paths)
 
 for path in paths:
     temp_path = "/tmp/objs/" + path
-    bpy.ops.import_scene.obj(filepath=temp_path, axis_forward='-Y', axis_up='Z')
+    bpy.ops.import_scene.obj(filepath=temp_path, axis_forward='Y', axis_up='Z')
 
 objects = list(bpy.data.objects)
 for object in objects:
@@ -32,6 +32,7 @@ for object in objects:
     bpy.context.view_layer.objects.active.hide_viewport = True
     bpy.context.active_object.keyframe_insert(data_path = "hide_render", frame = frame + 1.0)
     bpy.context.active_object.keyframe_insert(data_path = "hide_viewport", frame = frame + 1.0)
+    bpy.context.active_object.show_wire = True
     frame += 1.0
 
 bpy.data.scenes['Scene'].frame_end = frame
